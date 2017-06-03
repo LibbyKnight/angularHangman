4c12b93b04b20d6bcc653fa21bed8cdb2c44f5ba
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-06-03 16:29:10 -0400

    hangman logic started in controller

--- public/states/main.controller.js
+++ public/states/main.controller.js
@@ -2,7 +2,20 @@ angular
 	.module('angularHangman.main')
 	.controller('MainCtrl', MainCtrl);
 
-function MainCtrl($http)  {
+function MainCtrl()  {
 		var vm = this;
+
+		var wordsArray = ["ipa", "stout", "lager", "porter", "pilsner", "weissbier", "cider"];
+		var randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+		var answerArray = [];
 		
+
+	function startGame () {
+
+		for (var i = 0; i < randomWord.length; i++) {
+		answerArray[i] = "_";
+	}
+
+	
+
 }
