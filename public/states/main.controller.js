b4df6fd3520bf0a80fd8113df957f4c1f6caef17
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-06-04 11:21:45 -0400

    added player lives logic

--- public/states/main.controller.js
+++ public/states/main.controller.js
@@ -3,19 +3,30 @@ angular
 	.controller('MainCtrl', MainCtrl);
 
 function MainCtrl()  {
-		var vm = this;
+		var vm = this; 
 
 		var wordsArray = ["ipa", "stout", "lager", "porter", "pilsner", "weissbier", "cider"];
 		var randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 		var answerArray = [];
-		
+		var guess;
+		var lives;
+	
+var showLives = document.getElementById("lives");
 
 	function startGame () {
-
+		//Set word spaces
 		for (var i = 0; i < randomWord.length; i++) {
 		answerArray[i] = "_";
 	}
 
-	
+	function playerLives () {
+		showLives.innerHTML = "You have " + lives + " lives";
+		if (lives < 1) {
+			showLives.innerHTML = "Game Over"
+			startGame();
+		}
+	}
+
 
 }
+
